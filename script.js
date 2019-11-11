05c90e6742f6953eb6aa88884ca33701266f880f
karenheyn <karenheyn@gmail.com>
2019-11-11 18:23:28 -0500

    fixed issue

--- script.js
+++ script.js
@@ -1,133 +1,156 @@
-const box = document.querySelectorAll('.box');
-const board = document.querySelector('.board');
-const button = document.querySelector('button')
-let player1Score = 0
-let player2Score = 0
-let player1 =[]
-let player2 =[]
-let MatchCheck = []
-let color = 'red'
-let click = 0
+const box = document.querySelectorAll(".box");
+const board = document.querySelector(".board");
+const button = document.querySelector("button");
+let player1Score = 0;
+let player2Score = 0;
+let player1 = [];
+let player2 = [];
+let MatchCheck = [];
+let color = "red";
+let click = 0;
 
 for (let i = 0; i < box.length; i++) {
-    box[i].dataset.isClicked = false
-    box[i].addEventListener('click', function (evt) {
-        evt.preventDefault()
-        if (evt.target.dataset.isClicked == 'false') {
-            evt.target.dataset.isClicked = true
-            if (color == 'red') {
-                evt.target.style.backgroundColor = 'red'
-                color = 'blue'
-                click ++
-                console.dir(evt.target.dataset.number)
-    
-            } else if (color == 'blue') {
-                evt.target.style.backgroundColor = 'blue'
-                color = 'red'
-                click ++
-            }
-        }
-        if (click === 9) {
-            alert('its a tie')
-        }else{
+  box[i].dataset.isClicked = false;
+  box[i].addEventListener("click", function(evt) {
+    evt.preventDefault();
+    if (evt.target.dataset.isClicked == "false") {
+      evt.target.dataset.isClicked = true;
+      if (color == "red") {
+        evt.target.style.backgroundColor = "red";
+        color = "blue";
+        player1.push(i);
         checkForWinner();
-        }
-    })
-}
-function checkForWinner(){
-    if (box[0].style.backgroundColor === box[1].style.backgroundColor && box[1].style.backgroundColor === box[2].style.backgroundColor){
-       if(box[0].style.backgroundColor == 'red'){
-           player1Score++;
-           alert('player1 wins');
-       }
-        if(box[0].style.backgroundColor == 'blue'){
-           player2Score++;
-           alert('player2 wins');
-       }
+        click++;
+        console.dir(evt.target.dataset.number);
+      } else if (color == "blue") {
+        evt.target.style.backgroundColor = "blue";
+        color = "red";
+        player2.push(i);
+        checkForWinner();
+        click++;
+      }
     }
-    else if (box[3].style.backgroundColor === box[4].style.backgroundColor && box[4].style.backgroundColor === box[5].style.backgroundColor){
-        if(box[3].style.backgroundColor === 'red'){
-            player1Score++;
-            alert('player1 wins second statement');
-        }
-        if(box[3].style.backgroundColor === 'blue'){
-            player2Score++;
-            alert('player2 wins');
-        }
-     }
-     else if (box[6].style.backgroundColor === box[7].style.backgroundColor && box[7].style.backgroundColor === box[8].style.backgroundColor){
-        if(box[6].style.backgroundColor === 'red'){
-            player1Score++;
-            alert('player1 wins');
-        }
-        if(box[6].style.backgroundColor === 'blue'){
-            player2Score++;
-            alert('player2 wins');
-        }
-     }
-     else if (box[0].style.backgroundColor === box[3].style.backgroundColor && box[3].style.backgroundColor === box[6].style.backgroundColor){
-        if(box[0].style.backgroundColor === 'red'){
-            player1Score++;
-            alert('player1 wins');
-        }
-        if(box[0].style.backgroundColor === 'blue'){
-            player2Score++;
-            alert('player2 wins');
-        }
-     }
-     else if (box[1].style.backgroundColor === box[4].style.backgroundColor && box[4].style.backgroundColor === box[7].style.backgroundColor){
-        if(box[1].style.backgroundColor === 'red'){
-            player1Score++;
-            alert('player1 wins');
-        }
-        if(box[1].style.backgroundColor === 'blue'){
-            player2Score++;
-            alert('player2 wins');
-        }
-     }
-     else if (box[2].style.backgroundColor === box[5].style.backgroundColor && box[5].style.backgroundColor === box[8].style.backgroundColor){
-        if(box[2].style.backgroundColor === 'red'){
-            player1Score++;
-            alert('player1 wins');
-        }
-        if(box[2].style.backgroundColor === 'blue'){
-            player2Score++;
-            alert('player2 wins');
-        }
-     }
-     else if (box[0].style.backgroundColor === box[4].style.backgroundColor && box[4].style.backgroundColor === box[8].style.backgroundColor){
-        if(box[0].style.backgroundColor === 'red'){
-            player1Score++;
-            alert('player1 wins');
-        }
-        if(box[0].style.backgroundColor === 'blue'){
-            player2Score++;
-            alert('player2 wins');
-        }
-     }
-     else if (box[2].style.backgroundColor === box[4].style.backgroundColor && box[4].style.backgroundColor === box[6].style.backgroundColor){
-        if(box[2].style.backgroundColor === 'red'){
-            player1Score++;
-            alert('player1 wins');
-        }
-        if(box[2].style.backgroundColor === 'blue'){
-            player2Score++;
-            alert('player2 wins');
-        }
-        
-     }
-    
+    // if (
+    //   (click >= 9 && player1Score >= 5) ||
+    //   (click >= 9 && player1Score >= 5)
+    // ) {
+    //   alert("its a tie");
+    // } else {
+    //   checkForWinner();
+    // }
+  });
 }
-    button.addEventListener('click', function(evt){
-        for (i = 0; i < box.length; i++){
-        evt.preventDefault()
-        box[i].style.backgroundColor = '';
-        box[i].dataset.isClicked = false;
+function checkForWinner() {
+  if (
+    box[0].style.backgroundColor === box[1].style.backgroundColor &&
+    box[1].style.backgroundColor === box[2].style.backgroundColor
+  ) {
+    if (box[0].style.backgroundColor == "red") {
+      player1Score++;
+      alert("player1 wins");
     }
-
+    if (box[0].style.backgroundColor == "blue") {
+      player2Score++;
+      alert("player2 wins");
+    }
+  } else if (
+    box[3].style.backgroundColor === box[4].style.backgroundColor &&
+    box[4].style.backgroundColor === box[5].style.backgroundColor
+  ) {
+    if (box[3].style.backgroundColor === "red") {
+      player1Score++;
+      alert("player1 wins second statement");
+    }
+    if (box[3].style.backgroundColor === "blue") {
+      player2Score++;
+      alert("player2 wins");
+    }
+  } else if (
+    box[6].style.backgroundColor === box[7].style.backgroundColor &&
+    box[7].style.backgroundColor === box[8].style.backgroundColor
+  ) {
+    if (box[6].style.backgroundColor === "red") {
+      player1Score++;
+      alert("player1 wins");
+    }
+    if (box[6].style.backgroundColor === "blue") {
+      player2Score++;
+      alert("player2 wins");
+    }
+  } else if (
+    box[0].style.backgroundColor === box[3].style.backgroundColor &&
+    box[3].style.backgroundColor === box[6].style.backgroundColor
+  ) {
+    if (box[0].style.backgroundColor === "red") {
+      player1Score++;
+      alert("player1 wins");
+    }
+    if (box[0].style.backgroundColor === "blue") {
+      player2Score++;
+      alert("player2 wins");
+    }
+  } else if (
+    box[1].style.backgroundColor === box[4].style.backgroundColor &&
+    box[4].style.backgroundColor === box[7].style.backgroundColor
+  ) {
+    if (box[1].style.backgroundColor === "red") {
+      player1Score++;
+      alert("player1 wins");
+    }
+    if (box[1].style.backgroundColor === "blue") {
+      player2Score++;
+      alert("player2 wins");
+    }
+  } else if (
+    box[2].style.backgroundColor === box[5].style.backgroundColor &&
+    box[5].style.backgroundColor === box[8].style.backgroundColor
+  ) {
+    if (box[2].style.backgroundColor === "red") {
+      player1Score++;
+      alert("player1 wins");
+    }
+    if (box[2].style.backgroundColor === "blue") {
+      player2Score++;
+      alert("player2 wins");
+    }
+  } else if (
+    box[0].style.backgroundColor === box[4].style.backgroundColor &&
+    box[4].style.backgroundColor === box[8].style.backgroundColor
+  ) {
+    if (box[0].style.backgroundColor === "red") {
+      player1Score++;
+      alert("player1 wins");
+    }
+    if (box[0].style.backgroundColor === "blue") {
+      player2Score++;
+      alert("player2 wins");
+    }
+  } else if (
+    box[2].style.backgroundColor === box[4].style.backgroundColor &&
+    box[4].style.backgroundColor === box[6].style.backgroundColor
+  ) {
+    if (box[2].style.backgroundColor === "red") {
+      player1Score++;
+      alert("player1 wins");
+    }
+    if (box[2].style.backgroundColor === "blue") {
+      player2Score++;
+      alert("player2 wins");
+    }
+  } else if (player1.length >= 5 || player2.length >= 5) {
+    player1 = [];
+    player2 = [];
+    alert("its a tie");
+  }
 }
-    )
+button.addEventListener("click", function(evt) {
+  for (i = 0; i < box.length; i++) {
+    evt.preventDefault();
+    box[i].style.backgroundColor = "";
+    box[i].dataset.isClicked = false;
+  }
+});
 // button.addEventListener('click', function(evt){
 //     evt.preventDefault();
-    
+
 // })
